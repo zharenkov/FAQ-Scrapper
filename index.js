04d1977e866b98fecfde5cb531311ca685adabd9
John Naylor <jonaylor89@gmail.com>
2017-09-16 18:27:51 -0400

    rename file

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
 const Puppeteer = require('puppeteer');
-const FaqScrapper = require('./FaqScrapper.js');
+const ScrapeFaqs = require('./ScrapeFaq.js');
 const fs = require('fs');
 
 const target = ['https://www.ugrad.vcu.edu/why/faqs/admissions.html'];
-const output = '~/FAQ-Bot/FAQs.json';
+const output = 'FAQs.json';
 
 let faqs = {};
 
 const grabFAQs = function(question) {
-  Puppeteer.launch().then((browser) => {
+  Puppeteer.launch({headless: false}).then((browser) => {
     browser.newPage().then((page) => {
-      Promise.all(target.map(i => new Promise(function() {
-        page.goto(i).then((resolve, reject) => {
-          page.evaluate(function(FaqScrapper) {
-            'use strict';
-            let faq = (new FaqScrapper()).getFaqs();
+      Promise.all(target.map(i => new Promise((resolve, reject) => {
+        page.goto(i).then(() => {
+          page.evaluate(ScrapeFaqs).then(faq => {
             faqs = {
               faq,
               ...faqs
-            };
-            resolve();
-          }.bind(this, FaqScrapper));
+            }
+          });
+          resolve();
         }).catch(console.error);
       }))).then(i => {
         let faqStr = JSON.stringify(faqs);
+        console.log(faqStr);
         fs.writeFile(output, faqStr, console.error);
         browser.close();
       }).catch(console.error);
