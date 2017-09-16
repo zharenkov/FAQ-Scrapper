4903d5f58ade5fc2d12368f00bc5bdaf520a0274
John Naylor <jonaylor89@gmail.com>
2017-09-16 17:42:38 -0400

    implementing FAQ SCrapper

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 const Puppeteer = require('puppeteer');
-const FaqScapper = require('FaqScapper');
+const FaqScrapper = require('./FaqScrapper.js');
+const fs = require('fs');
 
-const target = 'https://www.ugrad.vcu.edu/why/faqs/admissions.html';
+const target = ['https://www.ugrad.vcu.edu/why/faqs/admissions.html'];
+const output = '~/FAQ-Bot/FAQs.json';
+
+let faqs = {};
 
 const grabFAQs = function(question) {
-  puppeteer.launch().then((browser) => {
+  Puppeteer.launch().then((browser) => {
     browser.newPage().then((page) => {
-      page.goto(target).then(() => {
-        page.evaluate(function() {
-          // return code
-        });
+      Promise.all(target.map(i => new Promise(function() {
+        page.goto(i).then((resolve, reject) => {
+          page.evaluate(function(FaqScrapper) {
+            'use strict';
+            let faq = (new FaqScrapper()).getFaqs();
+            faqs = {
+              faq,
+              ...faqs
+            };
+            resolve();
+          }.bind(this, FaqScrapper));
+        }).catch(console.error);
+      }))).then(i => {
+        let faqStr = JSON.stringify(faqs);
+        fs.writeFile(output, faqStr, console.error);
         browser.close();
-      });
-    });
-  });
+      }).catch(console.error);
+    }).catch(console.error);
+  }).catch(console.error);
 };
+
+grabFAQs();
