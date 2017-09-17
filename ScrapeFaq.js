6747d101bc27aa44d82cdc2df00be773fade426d
John Naylor <jonaylor89@gmail.com>
2017-09-17 00:43:13 -0400

    New JSON format

--- ScrapeFaq.js
+++ ScrapeFaq.js
@@ -41,10 +41,11 @@ module.exports = function ScrapeFaq() {
           .filter(i => i.getBoundingClientRect().x >= minX
             && i.getBoundingClientRect().x <= maxX
             && i.getBoundingClientRect().y > minY
-            && i.getBoundingClientRect().y <= maxY);
+            && i.getBoundingClientRect().y < maxY
+            && questions.filter(questionElement => questionElement.contains(i)).length == 0);
         if (elements.map(i => i.innerText.trim()).join('').length > 1) {
           questionAnswers.push({
-            question: e.innerText.trim(),
+            question: e.innerText.toLowerCase().replace(/[^\w\']/g, ' ').trim(),
             answer: elements.map(i => i.innerText.trim()).join('\n'),
             source: location.host
           });
